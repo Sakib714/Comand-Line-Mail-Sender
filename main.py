25b2a257d78c0c78fe2831d28b224e7885cc1740
Sadman Sakib <78845308+Sakib714@users.noreply.github.com>
2022-04-25 15:50:29 +0600

    improved security

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 587)
 
 server.starttls()
-server.login("sakibsadman546@gmail.com", "SAKIB@own@01")
+server.login(mail_address, password)
 
-server.sendmail("sakibsadman546@gmail.com", "sakibsadman546@gmail.com", 'mail send from my python code')
+server.sendmail(from_mail_address, to_mail_address, mail_text)
 
 print('Mail Send')
